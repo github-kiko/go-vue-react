299d35266b2b08d1f528aebe2cbe13a6c8ff5878
fangdongdong <1179086761@qq.com>
2023-03-11 19:21:52 +0800

    后端-创建项目、引入相关依赖库

---
+++ server/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+// import (
+// 	"github.com/gin-gonic/gin"
+// 	"gorm.io/gorm"
+// 	"gorm.io/driver/sqlite"
+// 	"gorm.io/driver/sql"
+//   )
+
+
+func main() {
+	fmt.Println("hello，全栈工程师～")
+	
+}
+
