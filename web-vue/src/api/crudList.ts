7cacbc4fda4e1b6160c8cbea6a7fec4928fdba78
fangdongdong <1179086761@qq.com>
2023-03-31 10:19:14 +0800

    后端返回的data数据使用泛型，定义好结构体

--- web-vue/src/api/crudList.ts
+++ web-vue/src/api/crudList.ts
@@ -1,29 +1,29 @@
 import { AxiosResponse } from 'axios';
-import {ListResponse,ResponseData,listaddRequset, listdeleteRequset,listupdateRequset,listqueryRequset} from "../model/crudList";
+import {ListResponse,ResponseData,listaddRequset, listdeleteRequset,listupdateRequset,listqueryRequset,listqueryResponse} from "../model/crudList";
 import http from "../utils/http"
 
-// 新增
-  export async function listadd(params: listaddRequset): Promise<ListResponse> {
-    const response: AxiosResponse<ResponseData> = await http.post('/list/add',params);
+// 新
+  export async function listadd(params: listaddRequset): Promise<ListResponse<listqueryResponse>> {
+    const response: AxiosResponse<ResponseData<listqueryResponse>> = await http.post('/list/add',params);
     return response.data;
   }
 
 
  // 删除
-  export async function listdelete(params: listdeleteRequset): Promise<ListResponse> {
-    const response: AxiosResponse<ResponseData> = await http.delete(`/list/delete/${params.id}`,params);
+  export async function listdelete(params: listdeleteRequset): Promise<ListResponse<listqueryResponse>> {
+    const response: AxiosResponse<ResponseData<listqueryResponse>> = await http.delete(`/list/delete/${params.id}`,params);
     return response.data;
 }
 
 // 修改
- export async function listupdate(params: listupdateRequset): Promise<ListResponse> {
-    const response: AxiosResponse<ResponseData> = await http.put(`/list/update/${params.id}`,params);
+ export async function listupdate(params: listupdateRequset): Promise<ListResponse<listqueryResponse>> {
+    const response: AxiosResponse<ResponseData<listqueryResponse>> = await http.put(`/list/update/${params.id}`,params);
     return response.data;
 }
 
 // 查询
-export async function listquery(params: listqueryRequset): Promise<ListResponse> {
-    const response: AxiosResponse<ResponseData> = await http.get('/list/query',params);
+export async function listquery(params: listqueryRequset): Promise<ListResponse<listqueryResponse>> {
+    const response: AxiosResponse<ResponseData<listqueryResponse>> = await http.get('/list/query',params);
     return response.data;
 }
 
--- web-vue/src/model/crudList.ts
+++ web-vue/src/model/crudList.ts
@@ -1,19 +1,27 @@
-
-
-// 后端返回
-export interface ListResponse{
+// 后端返回结构体
+export interface ListResponse<T>{
     code: number;
-    data: any;
+    data: T;
     total:number;
   }
 
-  export interface ResponseData{
+  export interface ResponseData<T>{
     code: number;
-    data: any;
+    data: T;
     total:number;
   }
 
-  // 前端请求
+
+  export interface listqueryResponse{
+    name: string;
+    age: number;
+    school: string;
+    phone: string;
+    address: string;
+  }
+
+
+  // 前端请求结构体
   export interface listaddRequset{
     name: string;
     age: number;
